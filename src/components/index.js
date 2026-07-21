49c688ba8560517496f301326be9a212b1b99b68
Sarthak Ahir <sarthakdahir@gmail.com>
2026-07-21 21:35:56 +0530

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+import Signup from "./Signup";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard" 
+import AuthLayout from "./AuthLayout";
+import Login from "./Login";
+
+export { 
+        Header, 
+        Footer, 
+        Container, 
+        Logo, 
+        LogoutBtn, 
+        RTE, 
+        Button, 
+        Input, 
+        Select, 
+        Signup, 
+        PostForm,
+        PostCard,
+        AuthLayout,
+        Login
+    };
