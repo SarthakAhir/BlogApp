6ffb9a6a234ccacd3562b576bbb17be477ff991d
Sarthak Ahir <sarthakdahir@gmail.com>
2026-07-30 16:07:38 +0530

    {Re-Designing} - { update logout btn}

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,20 +1,26 @@
-import {useDispatch} from 'react-redux';
-import authService from '../../appwrite/auth';
-import {logout} from '../../store/authSlice';
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/auth";
+import { logout } from "../../store/authSlice";
+import { Button } from "../index";
 
 function LogoutBtn() {
-    const dispatch = useDispatch();
-    const logoutHandler = () => {
-        authService.logout().then(() => {
-            dispatch(logout());
-        })
-    }
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    authService.logout().then(() => {
+      dispatch(logout());
+    });
+  };
+
   return (
-      <button
-    className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-    onClick={logoutHandler}
-    >Logout</button>
-  )
+    <Button
+      variant="outline"
+      onClick={logoutHandler}
+      className="px-5 py-2"
+    >
+      Logout
+    </Button>
+  );
 }
 
-export default LogoutBtn
+export default LogoutBtn;
